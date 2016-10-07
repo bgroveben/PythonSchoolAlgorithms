500f9c25c509fa2979f9e5719fae7938b6ce96a2
bgroveben <bgroveben@gmail.com>
2016-10-07 02:17:24 -0700

    Write fundamental preprocessing definition for BM_search

--- searching_algorithms/BM_search.py
+++ searching_algorithms/BM_search.py
@@ -28,6 +28,44 @@ def match_length(S, idx1, idx2)
         idx2 += 2
     return match_count
 
-# The following function returns Z, the Fundamental Preproocessing of S.
+# The following function returns Z, the Fundamental Preprocessing of S.
 # Z[i] is the length of the substring beginning at i which is also a prefix of S.
 # This pre-processing is done in O(n) time, where n is the length of S.
+def fundamental_preprocess(S):
+    if len(S) == 0:  # handles empty strings
+        return []
+    if len(S) == 1:  # for single character strings
+        return [1]
+    z = [0 for x in S]
+    z[0] = len(S)
+    z[1] = match_length(S, 0, 1)
+    for i in range(2, 1+z[1]):
+        z[i] = z[1]-i+1
+    # define lower and upper limits of z-box
+    l = 0
+    r = 0
+    for i in range(2+z[1], len(S)):
+        if i <= r:  # i falls within existing z-box
+            k = i-1
+            b = z[k]
+            a = r-i+1
+            if b < a:  # b ends within existing z-box
+                z[i] = b
+            else:  # b ends at or after the end of the z-box, so we need to do an explicit match to the right of the z-box
+                z[i] = a + match_length(S, a, r+1)
+                l = i
+                r = i+z[i]-1
+        else:  # i does not reside within the existing z-box
+            z[i] = match_length(S, 0, i)
+            if z[i] > 0:
+                l = i
+                r = i+z[i]-1
+    return z
+
+# Generates R for S, which is an array indexed by the position of some character c in the English alphabet.
+# At that index in R is an array of length |S|+1, specifying for each index i in S (plus the index after S)
+# the next location of character c encountered when traversing S from right to left starting at i.
+# This is used for a constant-time lookup for the bad character rule in the Boyer-Moore string search algorithm,
+# although it has a much larger size than non-constant-time solutions.
+def bad_character_table(S):
+    pass
